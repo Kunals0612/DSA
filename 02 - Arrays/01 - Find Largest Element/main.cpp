3bb0ce3fecfa611e4a6da515dd05f90a98a1af65
Kunals <146503735+Kunals0612@users.noreply.github.com>
2025-05-14 08:47:36 +0000

    commit

---
+++ 02 - Arrays/01 - Find Largest Element/main.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+        int findLargest(vector<int>& nums){
+            int largest = INT_MIN;
+            int n = nums.size();
+            for(int i = 0; i < n; i++){
+                if(nums[i] > largest){
+                    largest = nums[i];
+                }
+            }
+            return largest
+        }
+};
---
+++ 02 - Arrays/02 - Find Second Largest/main.cpp
@@ -0,0 +1,6 @@
+class Solution{
+    public:
+        int findSecondLargest(vector<int>& nums){
+            
+        }
+};
