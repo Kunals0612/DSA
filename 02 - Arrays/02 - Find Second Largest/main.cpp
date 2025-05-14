bd842691e7b6f481b5ccb3ceef85551e4a43a0ed
Kunals <146503735+Kunals0612@users.noreply.github.com>
2025-05-14 08:55:27 +0000

    commit

--- 02 - Arrays/02 - Find Second Largest/main.cpp
+++ 02 - Arrays/02 - Find Second Largest/main.cpp
@@ -1,6 +1,20 @@
 class Solution{
     public:
         int findSecondLargest(vector<int>& nums){
-            
+            int n = nums.size();
+
+            int largest = INT_MIN;
+            int secondLargest = INT_MIN;
+
+            for(int i = 0; i < n; i++){
+                if(nums[i] > largest){
+                    secondLargest = largest;
+                    largest = nums[i];
+                }else if(nums[i] > secondLargest && nums[i] != largest){
+                    secondLargest = nums[i];
+                }
+            }
+
+            return secondLargest;
         }
 };
